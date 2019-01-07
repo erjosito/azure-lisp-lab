b948825e4190715a7b34bd1d50b41808c79d5494
Jose Moreno <jomore@microsoft.com>
2019-01-07 23:54:53 +0100

    parameter type object

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,2 +1,3 @@
 # Deploy lab
 az group create -n lisplab -l westeurope
+az group deployment create -g lisplab --template-uri https://raw.githubusercontent.com/erjosito/azure-lisp-lab/master/lisp_lab.json --parameters '{"admin":{"value":"Microsoft123!"}}'
