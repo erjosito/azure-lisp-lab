d471c53b042ea594fb8f1fff1ff2b9ea30065d14
Jose Moreno <jomore@microsoft.com>
2019-01-08 00:52:04 +0100

    custom script extension

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -1,4 +1,11 @@
 # Deploy lab
-az group create -n lisplab -l westeurope
 url=https://raw.githubusercontent.com/erjosito/azure-lisp-lab/master/lisp_lab.json
-az group deployment create -g lisplab --template-uri $url --parameters '{"adminPassword":{"value":"Microsoft123!"}}'
+rg=lisplab
+az group create -n $rg -l westeurope
+az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"Microsoft123!"}}'
+
+# Check Public IP addresses
+az network public-ip list -g $rg --query [].[name,ipAddress] -o tsv
+
+# Cleanup
+az group delete -n $rg -y --no-wait
